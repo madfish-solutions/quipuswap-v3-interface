494f54f0c62b5284580c2204e9a57d6dfdfeeeea
cyace84 <cyace84@gmail.com>
2022-11-03 14:37:43 +0100

    added fa2Storage type d

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -41,6 +41,19 @@ export declare namespace fa2Types {
     } | {
         remove_operator: Operator;
     };
+    type FA2Storage = {
+        account_info: MichelsonMap<MichelsonMapKey, unknown>;
+        token_info: MichelsonMap<MichelsonMapKey, unknown>;
+        metadata: MichelsonMap<MichelsonMapKey, unknown>;
+        token_metadata: MichelsonMap<MichelsonMapKey, unknown>;
+        minters_info: MichelsonMap<MichelsonMapKey, unknown>;
+        last_token_id: BigNumber;
+        admin: string;
+        permit_counter: BigNumber;
+        permits: MichelsonMap<MichelsonMapKey, unknown>;
+        default_expiry: BigNumber;
+        total_minter_shares: BigNumber;
+    };
 }
 export declare namespace quipuswapV3Types {
     type x80n = {
