2222f3f1900a7f50a00b490b205d87da97518a5d
cyace84 <cyace84@gmail.com>
2022-11-03 14:52:25 +0100

    remove trash

--- dist/ss.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { validateAddress } = require("@taquito/utils");
-// const s = new Address("tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb");
-console.log(validateAddress("tz2VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb"));
